2bbe46ad551ea0bb94f591b819e4e1943cbe68b2
Connor Reaumond <creaumond@gmail.com>
2017-01-11 12:48:02 -0600

    more debugging

--- app/assets/javascripts/referralToggle.js
+++ app/assets/javascripts/referralToggle.js
@@ -1,4 +1,5 @@
 $(function() {
+  console.log('hey')
   $('input[type=radio][name=job\\[referral_type_id\\]]').change(function() {
       var $input = $('input[type=radio][name=job\\[referral_type_id\\]]:checked');
       var referralType = $('label[for=' + $input.attr('id') + ']').text();
