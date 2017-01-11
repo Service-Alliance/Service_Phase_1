87f414fef435c01690bd5c9dc3f4035bafa7c826
Connor Reaumond <creaumond@gmail.com>
2017-01-11 12:55:13 -0600

    Remove logs

--- app/assets/javascripts/referralToggle.js
+++ app/assets/javascripts/referralToggle.js
@@ -1,13 +1,9 @@
 $(function() {
-  console.log('hey')
   $('input[type=radio][name=job\\[referral_type_id\\]]').change(function() {
       var $input = $('input[type=radio][name=job\\[referral_type_id\\]]:checked');
       var referralType = $('label[for=' + $input.attr('id') + ']').text();
-      var escaped_referralType = referralType.replace(/([ #/.?*+^$[\]\\(){}|-])/g, "\\$1");
-      console.log(referralType)
       if (referralType == "Servpro Employee") {
         $("#referral_employee").removeClass("hidden");
-        console.log('yooo')
       }
       else {
         $("#referral_employee").addClass("hidden");
