02387863b4aa0f86312c998ec7599e8107ea01b4
Connor Reaumond <creaumond@gmail.com>
2017-01-26 14:18:12 -0600

    Update call parser to take recordings for existing calls

--- app/models/call.rb
+++ app/models/call.rb
@@ -16,9 +16,13 @@ class Call < ActiveRecord::Base
     calls.each do |call|
       # Check to see if record exists before creating
       existing = Call.find_by(callrail_id: call['id'])
-      unless existing
-        Call.create(callrail_id: call['id'], recording: call['recording_player'], duration: call['duration'], start_time: call['start_time'], customer_phone_number: call['caller_number'])
+      if existing
+        existing.recording == call['recording_player']
+        existing.save
       end
+      # unless existing
+      #   Call.create(callrail_id: call['id'], recording: call['recording_player'], duration: call['duration'], start_time: call['start_time'], customer_phone_number: call['caller_number'])
+      # end
     end
   end
 
