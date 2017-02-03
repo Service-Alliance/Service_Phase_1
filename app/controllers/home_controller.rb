107f3e99a108395189033812ef1f5db01ea1d7e8
Connor Reaumond <creaumond@gmail.com>
2017-02-03 12:21:10 -0600

    Front end updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @active_leads = Job.where(job_status: 1).count
     @active_jobs = Job.where(job_status: 2).count
     @month_calls = Call.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month).count
+    @ongoing_calls = Call.where(inprogress: true).count
 
 
     @activities = PublicActivity::Activity.all
