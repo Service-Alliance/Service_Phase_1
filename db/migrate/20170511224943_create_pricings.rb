3ae5ce709e7c01b7ee86f134b925227764206851
Connor Reaumond <creaumond@gmail.com>
2017-05-12 09:17:32 -0500

    Pricing Update

--- db/migrate/20170511224943_create_pricings.rb
+++ db/migrate/20170511224943_create_pricings.rb
@@ -1,9 +1,9 @@
 class CreatePricings < ActiveRecord::Migration
   def change
     create_table :pricings do |t|
-      t.decimal :price
+      t.decimal :price, :precision => 8, :scale => 2
       t.integer :pricing_category_id
-      t.text :description, :precision => 8, :scale => 2
+      t.text :description
 
       t.timestamps null: false
     end
