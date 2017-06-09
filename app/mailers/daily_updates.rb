c3b3348906aa83e107ac70f81ee0911ee827b9c9
Connor <creaumond@gmail.com>
2017-06-09 08:51:31 -0500

    Proper emails for notifications

--- app/mailers/daily_updates.rb
+++ app/mailers/daily_updates.rb
@@ -1,6 +1,6 @@
 class DailyUpdates < ApplicationMailer
   def all_pending
-    @user = User.find_by(email: "creaumond@gmail.com")
+    @user = User.find_by(email: "aott@servpro5933.com")
     @jobs = Job.where(status_id: 1)
     mail(to: @user.email, subject: "Pending Jobs as of (#{Date.today})")
   end
@@ -18,11 +18,11 @@ class DailyUpdates < ApplicationMailer
 
   def new_york_pending
     @jobs = Job.where(status_id: 1, franchise_id: [1, 3, 5, 6])
-    mail(to: 'creaumond@gmail.com', subject: "Pending Jobs as of (#{Date.today})")
+    mail(to: 'davidkluger1@gmail.com', subject: "Pending Jobs as of (#{Date.today})")
   end
 
   def new_jersey_pending
     @jobs = Job.where(status_id: 1, franchise_id: [2, 4])
-    mail(to: 'creaumond@gmail.com', subject: "Pending Jobs as of (#{Date.today})")
+    mail(to: 'meyerservpro@gmail.com', subject: "Pending Jobs as of (#{Date.today})")
   end
 end
