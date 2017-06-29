ba7453b2a4dd4317d5ed0073a0ccae29a8cac926
Deploy <deploy@deploy.com>
2017-06-29 08:32:10 -0500

    Update old URLS to new URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     :exception_recipients => %w{creaumond@gmail.com}
   }
 
-    config.action_mailer.default_url_options = { :host => 'servicealliance.herokuapp.com' }
+    config.action_mailer.default_url_options = { :host => 'digitaljobchart.com' }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
