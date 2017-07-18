bdf3b1f14c35f0444d539499ac398e705044e590
Connor Reaumond <creaumond@gmail.com>
2017-07-17 23:50:50 -0500

    Updates to phones and user phone fields

--- app/assets/javascripts/phoneFormatter.js
+++ app/assets/javascripts/phoneFormatter.js
@@ -1,5 +1,5 @@
 $(function() {
-    var input = $('#phones_numbers_')
+    var input = $('.phone-format')
     input.mobilePhoneNumber({allowPhoneWithoutPrefix: '+1'});
       input.bind('country.mobilePhoneNumber', function(e, country) {
         $('.country').text(country || '')
