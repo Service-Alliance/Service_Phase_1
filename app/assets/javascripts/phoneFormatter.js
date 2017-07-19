af24de53895159172117c920c4bee7560d46ff2b
Connor Reaumond <creaumond@gmail.com>
2017-07-18 22:18:49 -0500

    Show on jobs new page

--- app/assets/javascripts/phoneFormatter.js
+++ app/assets/javascripts/phoneFormatter.js
@@ -1,7 +1,7 @@
-$(function() {
-    var input = $('.phone-format')
-    input.mobilePhoneNumber({allowPhoneWithoutPrefix: '+1'});
-      input.bind('country.mobilePhoneNumber', function(e, country) {
-        $('.country').text(country || '')
-      })
-});
+// $(function() {
+//     var input = $('.phone-format')
+//     input.mobilePhoneNumber({allowPhoneWithoutPrefix: '+1'});
+//       input.bind('country.mobilePhoneNumber', function(e, country) {
+//         $('.country').text(country || '')
+//       })
+// });
