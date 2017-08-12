7dc4a6a0f3059069fc20ebde1b26957f35948b7c
Stanislav Katkov <lunaticman@gmail.com>
2017-08-12 15:32:13 +0700

    Improve project setup process  (#21)
    * first draft for README.md
    
    * just small corrections to readme
    
    * we don't need AWS locally
    
    * improve README.md
    
    * Fix CarrierWave storage issues
    
    * Add Staging Instructions to README
    
    * Update README.md

--- app/uploaders/upload_uploader.rb
+++ app/uploaders/upload_uploader.rb
@@ -8,7 +8,8 @@ class UploadUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+
+  storage SW_STORAGE
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,15 +1,22 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                     # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],            # required
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],                        # required
-    region:                'us-west-2',
-    path_style:            true            
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = 'servicealliance'                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  SW_STORAGE = Rails.env.production? ? :fog : :file
+
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'                     # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV.fetch['AWS_ACCESS_KEY'],            # required
+      aws_secret_access_key: ENV.fetch['AWS_SECRET_KEY'],                        # required
+      region:                'us-west-2',
+      path_style:            true
+
+    }
+    config.fog_directory  = 'servicealliance'                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  elsif Rails.env.test?
+    config.enable_processing = false
+  end
 end
+
+
