a539785c9146728afb7668a71e16f822967588a8
Sam Peacey <sam.peacey@gmail.com>
2017-10-05 12:32:09 +0900

    do not send emails to scheduling manager if dept is construction and vendors present

--- test/services/work_order_delivery_service_test.rb
+++ test/services/work_order_delivery_service_test.rb
@@ -14,4 +14,26 @@ class WorkOrderDeliveryServiceTest < ActiveSupport::TestCase
     WorkOrderDeliveryService.new(@work_order, @current_user).deliver!
     assert_enqueued_jobs 3
   end
+
+  test 'delivers to the scheduling manager normally' do
+    WorkOrderDeliveryService.new(@work_order, @current_user).deliver!
+    assert mail_enqueued_for_user(users(:scheduling_manager))
+  end
+
+  test 'does not send to scheduling manager if dept Construction and vendors present' do
+    @work_order.vendors << vendors(:one)
+    @current_user.update_attribute :department_id, departments(:construction).id
+    WorkOrderDeliveryService.new(@work_order, @current_user).deliver!
+    assert no_mail_enqueued_for_user(users(:scheduling_manager))
+  end
+
+  def mail_enqueued_for_user(user)
+    user_id = user.to_global_id.to_s
+    jobs = enqueued_jobs.select{ |j| j[:args][3]["_aj_globalid"] == user_id }
+    jobs.size > 0
+  end
+
+  def no_mail_enqueued_for_user(user)
+    !mail_enqueued_for_user(user)
+  end
 end
