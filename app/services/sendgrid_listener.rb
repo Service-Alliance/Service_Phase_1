c337367f8baceb9c3a83d8f9a3a73b778e18b267
Stanislav K <lunaticman@gmail.com>
2017-10-12 01:47:04 +0700

    Adding SendgridListener, it records all emails for work_order

--- app/services/sendgrid_listener.rb
+++ app/services/sendgrid_listener.rb
@@ -1,7 +1,24 @@
+require_relative '../../lib/single_call_object'
+
 class SendgridListener
-  def self.call(data)
+  include Servpro::SingleCallObject
+
+  def call(data)
     if data['origin_type'] && data['origin_type'].eql?('work_order') && data['origin_id']
-      # do something with WorkOrder here
+      begin
+        order = WorkOrder.find(data['origin_id'])
+        order.events << event_builder(data)
+        order.save!
+      rescue ActiveRecord::RecordNotFound => e
+        binding.pry
+        Honeybadger.notify(e, context: data)
+      end
     end
+  end
+
+  private
+
+  def event_builder(data)
+    "#{DateTime.now} | Email to #{data['email']} was #{data['event']} by IP #{data['ip']}"
   end
 end
---
+++ test/services/sendgrid_listener_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class SendgridListenerTest < ActiveSupport::TestCase
+  test '#call should update events for WorkOrder' do
+    event = event_stores(:sendgrid)
+    order = WorkOrder.create!(work_orders(:one).attributes.merge(id:event.data['origin_id']))
+
+    assert_difference "WorkOrder.find(#{event.data['origin_id']}).events.count" do
+      SendgridListener.call(event.data)
+    end
+
+    order.delete
+  end
+
+
+  test 'if WorkOrder is missing' do
+    event = event_stores(:sendgrid)
+
+    assert_difference 'Honeybadger::Backend::Test.notifications.count' do
+      SendgridListener.call(event.data)
+    end
+  end
+end
