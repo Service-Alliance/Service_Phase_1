b53234bb09e0e8d22bbc30ef106eff2c664cf149
Stanislav K <lunaticman@gmail.com>
2017-10-12 21:26:42 +0700

    changes according to proposal

--- app/models/franchise_zipcode.rb
+++ app/models/franchise_zipcode.rb
@@ -1,11 +1,11 @@
 class FranchiseZipcode < ActiveRecord::Base
   belongs_to :franchise
 
-  validates_uniqueness_of :zip_code, conditions: -> { where.not(assigned: false)}
+  validates_uniqueness_of :zip_code, conditions: -> { assigned }
 
-  scope :active, -> { where(assigned: true) }
+  scope :assigned, -> { where(assigned: true) }
 
   def self.detect_franchise(zipcode)
-    active.where(zip_code: zipcode).pluck(:franchise_id).first
+    assigned.where(zip_code: zipcode).pluck(:franchise_id).first
   end
 end
