a5a2600528030f9a45e1a0a62687eeb35851a641
Stanislav Katkov <lunaticman@gmail.com>
2017-10-14 07:30:32 +0700

    fix SendgridListener (#117)

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -3,6 +3,6 @@ Gridhook.configure do |config|
 
   config.event_processor = proc do |event|
     EventStore.sendgrid(event.attributes)
-    SendgridListener.delay.call(event.attributes)
+    SendgridListener.call(event.attributes)
   end
 end
