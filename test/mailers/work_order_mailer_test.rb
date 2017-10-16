a73ce71333e54f739f2636a93e9a5cc4a8dd64d3
Brad Bergeron <bergeron.bradley@gmail.com>
2017-10-16 15:20:28 -0400

    change method in work order test from location to job_location

--- test/mailers/work_order_mailer_test.rb
+++ test/mailers/work_order_mailer_test.rb
@@ -19,7 +19,7 @@ class WorkOrderMailerTest < ActiveSupport::TestCase
         assert_equal @work_order.id, header_field["origin_id"]
       end
 
-      assert_includes email.body.raw_source, "Job Location: #{@work_order.location}"
+      assert_includes email.body.raw_source, "Job Location: #{@work_order.job_location}"
     end
   end
 
@@ -35,7 +35,7 @@ class WorkOrderMailerTest < ActiveSupport::TestCase
         assert_equal @work_order.id, header_field["origin_id"]
       end
 
-      assert_includes email.body.raw_source, "Job Location: #{@workorder.location}"
+      assert_includes email.body.raw_source, "Job Location: #{@workorder.job_location}"
     end
   end
 end
