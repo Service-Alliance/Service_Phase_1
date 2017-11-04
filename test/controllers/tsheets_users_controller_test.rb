a99415d007a219654bb2378dcd987914b316962e
Sam Peacey <sam.peacey@gmail.com>
2017-11-04 10:18:41 +0900

    fixed odd tsheets test error

--- test/controllers/tsheets_users_controller_test.rb
+++ test/controllers/tsheets_users_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 require_relative '../../app/controllers/tsheets/users_controller'
 
 class Tsheets::UsersControllerTest < ActionController::TestCase
-  setup do
+  def setup
     @user = users(:with_tsheet)
     sign_in(@user)
   end
 
   test "#update" do
-    patch :update, format: :json, id: @user
+    patch :update, format: :json, id: @user.id
     assert_response :success
   end
 end
