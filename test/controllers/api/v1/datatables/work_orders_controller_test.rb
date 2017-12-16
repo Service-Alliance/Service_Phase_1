9955bbc301823fe22a3434dfc1f20d461b608664
Sam Peacey <sam.peacey@gmail.com>
2017-12-16 12:07:15 +0900

    added test for searching work orders

--- test/controllers/api/v1/datatables/work_orders_controller_test.rb
+++ test/controllers/api/v1/datatables/work_orders_controller_test.rb
@@ -9,4 +9,11 @@ class Api::V1::Datatables::WorkOrdersControllerTest < ActionController::TestCase
     get :index, format: :json
     assert_response :success
   end
+
+  test '#index search' do
+    get :index, format: :json, search: 'one'
+    assert_response :success
+    assert_match /Work Order One/, response.body
+    refute_match /Work Order Two/, response.body
+  end
 end
