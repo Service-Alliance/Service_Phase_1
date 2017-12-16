6399676e20c8691cfb4b9d153d5066c69c44e4db
Sam Peacey <sam.peacey@gmail.com>
2017-12-16 11:57:26 +0900

    tests for user searches

--- test/controllers/api/v1/datatables/users_controller_test.rb
+++ test/controllers/api/v1/datatables/users_controller_test.rb
@@ -9,4 +9,11 @@ class Api::V1::Datatables::UsersControllerTest < ActionController::TestCase
     get :index, format: :json
     assert_response :success
   end
+
+  test '#index search' do
+    get :index, format: :json, search: 'dave'
+    assert_response :success
+    assert_match /Dave/, response.body
+    refute_match /Barry/, response.body
+  end
 end
