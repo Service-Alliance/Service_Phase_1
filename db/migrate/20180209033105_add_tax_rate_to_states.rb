24f46720a44a55f44729c23a1c5b6b74adf48000
Sam Peacey <sam.peacey@gmail.com>
2018-02-09 12:50:12 +0900

    added tax rate to states

--- db/migrate/20180209033105_add_tax_rate_to_states.rb
+++ db/migrate/20180209033105_add_tax_rate_to_states.rb
@@ -1,5 +1,5 @@
 class AddTaxRateToStates < ActiveRecord::Migration
   def change
-    add_column :states, :tax_rate, :decimal
+    add_column :states, :tax_rate, :decimal, null: false, default: 0
   end
 end
